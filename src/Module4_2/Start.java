6e4c83b4d94d71360e0de2c166254d8b31050f5a
Sergey Chesnokov <chesnokov_sergey9@mail.ru>
2024-02-17 12:13:01 +0300

    refactor 4.2.1: delete magic constant

--- src/Module4_2/Start.java
+++ src/Module4_2/Start.java
@@ -8,13 +8,13 @@ public class Start {
     }
 
     public static void moveRobot(RobotConnectionManager robotConnectionManager, int toX, int toY) {
-        for (int i = 0; i < 3; i++) {
-            try (RobotConnection robotConnection = robotConnectionManager.getConnection(i)) {
-                robotConnection.moveRobotTo(toX, toY, i);
-                i = 3;
-                break;
+        final int MAX_QUANTITY_TRY = 3;
+        for (int i = 0; i < MAX_QUANTITY_TRY; i++) {
+            try (RobotConnection robotConnection = robotConnectionManager.getConnection(i)) { //In the task, the method .getConnection() does not have a parameter "i", it is needed for tests
+                robotConnection.moveRobotTo(toX, toY, i); //In the task, the method .moveRobotTo(toX, toY) does not have a parameter "i", it is needed for tests
+                i = MAX_QUANTITY_TRY;
             } catch (RobotConnectionException connectionException) {
-                if (i == 2) {
+                if (i == MAX_QUANTITY_TRY - 1) {
                     throw connectionException;
                 }
             }
