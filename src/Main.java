5d0fc124c8baeeb22cebcd9c7d775d6c7beb3f6b
Sergey Chesnokov <chesnokov_sergey9@mail.ru>
2024-02-17 20:50:42 +0300

    init module 531

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,11 @@
-import Module5_2_2.Start;
-
+import Module5_3_1.Start;
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-//        Task4_3_2 task432 = new Task4_3_2();
-//        task432.start();
 
         Start.main(args);
-
-
     }
 }
 
---
+++ src/Module5_3_1/Start.java
@@ -0,0 +1,7 @@
+package Module5_3_1;
+
+public class Start {
+    public static void main(String[] args) {
+
+    }
+}
