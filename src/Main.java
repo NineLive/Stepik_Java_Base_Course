027d4a934022a26bd2aa650af8d60e3977aa44cd
Sergey Chesnokov <chesnokov_sergey9@mail.ru>
2024-02-17 16:41:28 +0300

    some diff

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
-import Module5_2.Start;
+import Module5_2_2.Start;
 
-import static Module4_1.Start.getCallerClassAndMethodName;
+import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 //        Task4_3_2 task432 = new Task4_3_2();
 //        task432.start();
 
-        Start.start();
+        Start.main(args);
 
 
     }
