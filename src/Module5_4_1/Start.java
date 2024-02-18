b42452c142fd531595a22e1f05ea827027b416d2
Sergey Chesnokov <chesnokov_sergey9@mail.ru>
2024-02-18 18:55:04 +0300

    complete task 5.4.1

--- src/Module5_4_1/Start.java
+++ src/Module5_4_1/Start.java
@@ -1,7 +1,36 @@
 package Module5_4_1;
 
-public class Start {
-    public static void main(String[] args) {
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+public class Start {
+    public static void main(String[] args) throws Exception {
+        Path path = Paths.get("D:\\2.bin");
+        Animal animal = new Animal("bobrkurwa", "2.5");
+
+        try (ObjectOutputStream oos = new ObjectOutputStream(Files.newOutputStream(path))) {
+            oos.writeObject(animal);
+        }
+        Animal deserializedBobr;
+        try (ObjectInputStream ois = new ObjectInputStream(Files.newInputStream(path))) {
+            deserializedBobr = (Animal) ois.readObject();
+
+        }
+        System.out.println(deserializedBobr.name);
+    }
+
+    public static Animal[] deserializeAnimalArray(byte[] data) {
+        try (ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(data))) {
+            int size = ois.readInt();
+            Animal[] result = new Animal[size];
+            for (int i = 0; i < result.length; i++) {
+                result[i] = (Animal) ois.readObject();
+            }
+            return result;
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
     }
 }
