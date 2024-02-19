9889191438029fcdd400ab84d2c80f9decd5fd36
Sergey Chesnokov <chesnokov_sergey9@mail.ru>
2024-02-19 20:21:19 +0300

    add new constructors

--- src/JokeTask/MyVisitor.java
+++ src/JokeTask/MyVisitor.java
@@ -10,11 +10,29 @@ import java.nio.file.attribute.BasicFileAttributes;
 
 
 public class MyVisitor extends SimpleFileVisitor<Path> {
-    public static final String JOKERGE = "public class joke { public static void main(String[] args) { System.out.println(\"hello world\"); } }";
+    public String textInFile;
+    public static final String FILE_NAME = "joke.java";
+
+    public MyVisitor() {
+        this.textInFile = "";
+    }
+
+    public MyVisitor(String textInFile) {
+        this.textInFile = textInFile;
+    }
+
+    /**
+     * Before visiting the directory this method creates a file called {@link  #FILE_NAME} containing {@link  #textInFile}
+     *
+     * @param dir   a reference to the directory
+     * @param attrs the directory's basic attributes
+     * @return nothing
+     * @throws IOException
+     */
     @Override
     public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-        try(FileWriter writer = new FileWriter(dir + File.separator + "joke.java")){
-            writer.write(JOKERGE);
+        try (FileWriter writer = new FileWriter(dir + File.separator + FILE_NAME)) {
+            writer.write(textInFile);
         }
         return FileVisitResult.CONTINUE;
     }
