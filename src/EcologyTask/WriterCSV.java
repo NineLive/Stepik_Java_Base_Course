3a00672081cd747283ce88ae9f76d79d3b6084b0
Sergey Chesnokov <chesnokov_sergey9@mail.ru>
2024-02-21 20:59:27 +0300

    refactoring, now create header and write him, delete data from class dataFromCSV

--- src/EcologyTask/WriterCSV.java
+++ src/EcologyTask/WriterCSV.java
@@ -1,27 +1,42 @@
 package EcologyTask;
 
 import java.io.*;
+import java.lang.reflect.Field;
+import java.nio.file.Path;
+import java.util.ArrayList;
 
 public class WriterCSV {
+    private static Path path;
+
+    WriterCSV(Path path) {
+        WriterCSV.path = path;
+    }
+
     /**
-     * Takes information from an instance of the DataFromCSV class and writes it to a new CSV file with the selected name.
-     *
-     * @param dataFromCSV
-     * @param nameNewFile
+     * Takes information from an instance of the DataFromCSV class and writes it to a new CSV file with the selected path.
      */
-    public static void writeEcologicalUser(DataFromCSV dataFromCSV, String nameNewFile) {
-        String newFilePath = dataFromCSV.getPath().getParent() + nameNewFile + ".csv";
+    public void writeNewCSV(ArrayList<User> ecoUsers) {
+        String newFilePath = path.toString();
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(newFilePath))) {
-            writer.write(dataFromCSV.header + "\n");
-            for (User user : dataFromCSV.users) {
-                if (user.isEcologyUser(dataFromCSV.maxConsumption)) {
-                    writer.write("%d|%s|%d|%d|%d|%d|%d\n".formatted(user.getId(), user.getName(),
-                            user.getWaterCount(), user.getGasCount1(), user.getElectroCount2(),
-                            user.getElectroCount1(), user.getElectroCount2()));
-                }
+            writer.write(createHeader());
+
+            for (User user : ecoUsers) {
+                writer.write("%d|%s|%d|%d|%d|%d|%d\n".formatted(user.getId(), user.getName(),
+                        user.getWaterCount(), user.getGasCount1(), user.getElectroCount2(),
+                        user.getElectroCount1(), user.getElectroCount2()));
             }
         } catch (IOException e) {
             throw new RuntimeException("Write exception", e);
         }
+    }
+
+    public static String createHeader() {
+        StringBuilder header = new StringBuilder();
+        for (Field field : User.class.getDeclaredFields()) {
+            System.out.println();
+            header.append(field.getName()).append("|");
+        }
+        header.append("\n");
+        return header.toString();
     }
 }
