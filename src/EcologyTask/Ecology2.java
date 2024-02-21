51c3b74710e96ccd166d522b345f4db5fce21208
Sergey Chesnokov <chesnokov_sergey9@mail.ru>
2024-02-21 20:54:46 +0300

    rewrite, add new method with lambda

--- src/EcologyTask/Ecology2.java
+++ src/EcologyTask/Ecology2.java
@@ -1,16 +1,33 @@
 package EcologyTask;
 
-
-import java.util.Arrays;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.function.Predicate;
 
 public class Ecology2 {
     public static void main(String[] args) {
-        makeEcoCSV( "D:\\data.csv",  701);
+//        makeEcoCSV(args[0], Integer.parseInt(args[1])); // PRODUCTION
+        makeEcoCSV("D:\\data.csv", 300); // DEBUG
     }
 
-    public static void makeEcoCSV(String path, int maxConsumption){
-        DataFromCSV dataFromCSV = new DataFromCSV(path, maxConsumption);
-        ReaderCSV.read(dataFromCSV);
-        WriterCSV.writeEcologicalUser(dataFromCSV,"dota2");
+    public static void makeEcoCSV(String fromFile, int maxConsumption) {
+        ReaderCSV readerCSV = new ReaderCSV(Paths.get(fromFile));
+        WriterCSV writerCSV = new WriterCSV(Paths.get("D:\\data2.csv"));
+        ArrayList<User> users = readerCSV.read();
+
+        ArrayList<User> ecoUsers = findEcologyUser(users, u -> (u.getWaterCount() <= maxConsumption) &&
+                (u.getAllGas() <= maxConsumption) && (u.getAllElectro() <= maxConsumption));
+
+        writerCSV.writeNewCSV(ecoUsers);
+    }
+
+    public static ArrayList<User> findEcologyUser(ArrayList<User> users, Predicate<User> filter) {
+        ArrayList<User> ecologyUsers = new ArrayList<>();
+        for (User user : users) {
+            if (filter.test(user)) {
+                ecologyUsers.add(user);
+            }
+        }
+        return ecologyUsers;
     }
 }
