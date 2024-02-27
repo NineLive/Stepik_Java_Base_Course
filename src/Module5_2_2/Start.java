4383826b353a145f6e34f7662049ef230e1d4230
Sergey Chesnokov <chesnokov_sergey9@mail.ru>
2024-02-27 23:31:40 +0300

    refactoring

--- src/Module5_2_2/Start.java
+++ src/Module5_2_2/Start.java
@@ -1,10 +1,10 @@
 package Module5_2_2;
 
 import java.io.*;
+import java.util.Collections;
+import java.util.stream.Collectors;
 
 public class Start {
-    public static final InputStream INPUT_STREAM = System.in;
-    public static final OutputStream OUTPUT_STREAM = System.out;
 
     public static void main(String[] args) throws IOException {
         byte[] mass = new byte[]{(byte) 65, (byte) 10, (byte) 10, (byte) 10, (byte) 10, (byte) 66};
@@ -12,29 +12,26 @@ public class Start {
         byte[] mass3 = new byte[]{};
 
         InputStream inputStream = new ByteArrayInputStream(mass2);
-
-        formatterWinToUnix(INPUT_STREAM);
+        String numbers = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sodales consectetur purus at faucibus. Donec mi quam, tempor vel ipsum non, faucibus suscipit massa. Morbi lacinia velit blandit tincidunt efficitur. Vestibulum eget metus imperdiet sapien laoreet faucibus. Nunc eget vehicula mauris, ac auctor lorem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel odio nec mi tempor dignissim.";
+        System.setIn(new ByteArrayInputStream(mass2));
+        formatterWinToUnix();
     }
+
     /**
      * The method replaces all occurrences of the pair of characters '\r' and '\n' with a single character '\n'.
      * If the input encounters a single '\r' character that is not followed by a '\n', then the '\r' character is
      * output unchanged. Characters '\r' has code 13 and '\n' has code 10
      */
-    public static void formatterWinToUnix(InputStream inputStream) throws IOException {
-        int[] buff = new int[2];
-        int firstByte = inputStream.read();
-        int nextByte;
-        if (firstByte != -1) {
-            buff[0] = firstByte;
-            while ((nextByte = inputStream.read()) != -1) {
-                buff[1] = nextByte;
-                if (!((buff[0] == 13) && (buff[1] == 10))) {
-                    OUTPUT_STREAM.write(buff[0]);
-                }
-                buff[0] = buff[1];
+    public static void formatterWinToUnix() throws IOException {
+        int previousValue = System.in.read();
+        int currentValue;
+        while (previousValue != -1) {
+            currentValue = System.in.read();
+            if (!(previousValue == '\r' && currentValue == '\n')) {
+                System.out.write(previousValue);
             }
-            OUTPUT_STREAM.write(buff[0]);
+            previousValue = currentValue;
         }
-        OUTPUT_STREAM.flush();
+        System.out.flush();
     }
 }
