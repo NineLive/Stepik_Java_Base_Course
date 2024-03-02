5790c103daa10946b5344a3501b865af7c4cb6c4
Sergey Chesnokov <chesnokov_sergey9@mail.ru>
2024-03-02 18:53:29 +0300

    complete bot task

---
+++ src/Dop_ReviewBotTask/Report.java
@@ -0,0 +1,37 @@
+package Dop_ReviewBotTask;
+
+import java.time.LocalDate;
+
+public class Report {
+
+    private Long id;
+
+    private String studentUserName;
+
+    private Integer hours;
+
+    private LocalDate date;
+
+    private String title;
+
+    public Report(long id, String studentUserName, Integer hours, LocalDate date, String title) {
+        this.id = id;
+        this.studentUserName = studentUserName;
+        this.hours = hours;
+        this.date = date;
+        this.title = title;
+    }
+
+    public String getStudentUserName() {
+        return studentUserName;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    @Override
+    public String toString() {
+        return studentUserName + "\n" + date + "\n" + hours + "\n" + title + "\n";
+    }
+}
